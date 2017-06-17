97b096432abc3071d828c4c8f4b31aecf35af823
Vineeth Chandran <vineeth.cnbr@gmail.com>
2017-06-18 00:58:38 +0530

    Demo

--- Server.js
+++ Server.js
@@ -26,6 +26,10 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
+/*app.listen(3000,function(){
   console.log("Live at Port 3000");
+});*/
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
